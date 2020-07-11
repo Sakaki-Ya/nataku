0fd60f6fc9a34efae268facfe7b9058c419f9ed3
Sakaki <aa31568@gmail.com>
2020-07-11 20:59:37 +0900

    リファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,12 @@ import style from "./styles/App.module.scss";
 import Posts from "./components/Posts";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { AuthProvider } from "./components/AuthContext";
 
 const App: React.FC = () => (
   <div className={style.app__wrap}>
-    <AuthProvider>
-      <Posts />
-      <Footer />
-      <Header />
-    </AuthProvider>
+    <Posts />
+    <Footer />
+    <Header />
     <ToastContainer />
   </div>
 );
